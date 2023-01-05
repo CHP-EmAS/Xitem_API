9fb8050a9cd8967658a14e9bb9e6109929946798
EmAS <clemens@huebner-clan.de>
2023-01-05 20:52:26 +0100

    try fix jwt decode

--- src_typescript/src/controllers/authController.ts
+++ src_typescript/src/controllers/authController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import * as jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import uuid from "uuid";
 
 import toObj from "../config/responseStandart"
